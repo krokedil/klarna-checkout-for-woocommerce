93c8497fbdc2e5c6b863ac0ad2cc0e7086966fa7
Slobodan Manic <slobodan.manic@gmail.com>
2017-11-09 12:56:17 +0100

    Updates how create account fields are shown in KCO page

--- includes/klarna-checkout-for-woocommerce-functions.php
+++ includes/klarna-checkout-for-woocommerce-functions.php
@@ -50,11 +50,32 @@ if ( ! function_exists( 'kco_wc_show_extra_fields' ) ) {
 			woocommerce_form_field( $key, $field, $key_value );
 		}
 
-		// Account.
-		foreach ( $extra_fields['account'] as $key => $field ) {
-			$key_value = array_key_exists( $key, $extra_fields_values ) ? $extra_fields_values[ $key ] : '';
-			woocommerce_form_field( $key, $field, $key_value );
-		}
+		if ( ! is_user_logged_in() && WC()->checkout()->is_registration_enabled() ) { ?>
+			<div class="woocommerce-account-fields">
+				<?php if ( ! WC()->checkout()->is_registration_required() ) { ?>
+					<p class="form-row form-row-wide create-account">
+						<label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
+							<input class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" id="createaccount" <?php checked( ( true === WC()->checkout()->get_value( 'createaccount' ) || ( true === apply_filters( 'woocommerce_create_account_default_checked', false ) ) ), true ) ?> type="checkbox" name="createaccount" value="1" /> <span><?php _e( 'Create an account?', 'woocommerce' ); ?></span>
+						</label>
+					</p>
+				<?php } ?>
+
+				<?php do_action( 'woocommerce_before_checkout_registration_form', WC()->checkout() ); ?>
+
+				<?php if ( WC()->checkout()->get_checkout_fields( 'account' ) ) { ?>
+
+					<div class="create-account">
+						<?php foreach ( WC()->checkout()->get_checkout_fields( 'account' ) as $key => $field ) { ?>
+							<?php woocommerce_form_field( $key, $field, WC()->checkout()->get_value( $key ) ); ?>
+						<?php } ?>
+						<div class="clear"></div>
+					</div>
+
+				<?php } ?>
+
+				<?php do_action( 'woocommerce_after_checkout_registration_form', WC()->checkout() ); ?>
+			</div>
+		<?php }
 
 		// Shipping.
 		foreach ( $extra_fields['shipping'] as $key => $field ) {
