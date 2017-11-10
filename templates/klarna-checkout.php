41d8554307cef8b67babf255a879c2ed39317617
Slobodan Manic <slobodan.manic@gmail.com>
2017-11-10 11:28:01 +0100

    Moves coupon form above checkout <form> in KCO page

--- templates/klarna-checkout.php
+++ templates/klarna-checkout.php
@@ -14,12 +14,14 @@ WC()->cart->calculate_totals();
 $available_gateways = WC()->payment_gateways()->get_available_payment_gateways();
 ?>
 
+<?php if ( count( $available_gateways ) > 1 ) { ?>
+	<p><a class="checkout-button button alt" href="#" id="klarna-checkout-select-other">Select another payment method</a></p>
+<?php } ?>
+
+<?php woocommerce_checkout_coupon_form(); ?>
+
 <form name="checkout" class="checkout woocommerce-checkout">
 	<div id="kco-wrapper">
-		<?php if ( count( $available_gateways ) > 1 ) { ?>
-			<p><a class="checkout-button button alt" href="#" id="klarna-checkout-select-other">Select another payment method</a></p>
-		<?php } ?>
-
 		<div id="kco-order-review">
 			<?php woocommerce_order_review(); ?>
 			<?php kco_wc_show_extra_fields(); ?>
