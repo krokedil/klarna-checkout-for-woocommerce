39c50da9fe59ee74559656d2bc9ae885ffdd4ea0
Slobodan Manic <slobodan.manic@gmail.com>
2017-11-30 12:58:27 +0100

    Adds can’t edit address note for all countries

--- includes/class-klarna-checkout-for-woocommerce-gateway.php
+++ includes/class-klarna-checkout-for-woocommerce-gateway.php
@@ -271,9 +271,7 @@ class Klarna_Checkout_For_WooCommerce_Gateway extends WC_Payment_Gateway {
 	 */
 	public function address_notice( $order ) {
 		if ( $this->id === $order->get_payment_method() ) {
-			if ( 'US' !== $order->get_billing_country() ) {
-				echo '<div style="margin: 10px 0; padding: 10px; border: 1px solid #B33A3A; font-size: 12px">Order address should not be changed and any changes you make will not be reflected in Klarna system.</div>';
-			}
+			echo '<div style="margin: 10px 0; padding: 10px; border: 1px solid #B33A3A; font-size: 12px">Order address should not be changed and any changes you make will not be reflected in Klarna system.</div>';
 		}
 	}
 
