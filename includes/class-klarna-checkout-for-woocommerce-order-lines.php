667dbbd2920d6f1c16df4f56c7b78858c86ffcd0
Niklas Högefjord <niklas@krokedil.se>
2018-04-25 15:20:26 +0200

    Temporary revert discount calculation change

--- includes/class-klarna-checkout-for-woocommerce-order-lines.php
+++ includes/class-klarna-checkout-for-woocommerce-order-lines.php
@@ -338,16 +338,10 @@ class Klarna_Checkout_For_WooCommerce_Order_Lines {
 	 * @return integer $item_price Cart item price.
 	 */
 	public function get_item_price( $cart_item ) {
-
 		if ( $this->separate_sales_tax ) {
 			$item_subtotal = $cart_item['line_subtotal'];
 		} else {
 			$item_subtotal = $cart_item['line_subtotal'] + $cart_item['line_subtotal_tax'];
-		}
-
-		$product = wc_get_product( $cart_item['product_id'] );
-		if ( ( floatval( $product->get_sale_price() ) * $cart_item['quantity'] ) === $item_subtotal ) {
-			$item_subtotal = floatval( $product->get_regular_price() ) * $cart_item['quantity'];
 		}
 		$item_price = $item_subtotal * 100 / $cart_item['quantity'];
 
@@ -411,8 +405,6 @@ class Klarna_Checkout_For_WooCommerce_Order_Lines {
 		} else {
 			$item_discount_amount = 0;
 		}
-		$product = wc_get_product( $cart_item['product_id'] );
-		$item_discount_amount = $item_discount_amount + ( ( ( floatval( $product->get_regular_price() ) - floatval( $product->get_price() ) ) * 100 ) * $cart_item['quantity'] );
 
 		return round( $item_discount_amount );
 	}
@@ -482,17 +474,6 @@ class Klarna_Checkout_For_WooCommerce_Order_Lines {
 			$item_total_amount = ( $cart_item['line_total'] * 100 );
 		} else {
 			$item_total_amount = ( ( $cart_item['line_total'] + $cart_item['line_tax'] ) * 100 );
-		}
-
-		$product = wc_get_product( $cart_item['product_id'] );
-		if ( $this->separate_sales_tax ) {
-			$item_subtotal = $cart_item['line_subtotal'];
-		} else {
-			$item_subtotal = $cart_item['line_subtotal'] + $cart_item['line_subtotal_tax'];
-		}
-		if ( ( floatval( $product->get_sale_price() ) * $cart_item['quantity'] ) === $item_subtotal ) {
-			$item_subtotal = floatval( $product->get_price() ) * $cart_item['quantity'];
-			$item_total_amount = $item_subtotal * 100;
 		}
 
 		return round( $item_total_amount );
