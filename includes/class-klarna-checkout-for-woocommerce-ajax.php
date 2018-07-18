ca2fa947712861770855d9a874d91fe5663e3285
Niklas Högefjord <niklas@krokedil.se>
2018-07-18 08:48:05 +0200

    PHP notice fix

--- includes/class-klarna-checkout-for-woocommerce-ajax.php
+++ includes/class-klarna-checkout-for-woocommerce-ajax.php
@@ -92,7 +92,7 @@ class Klarna_Checkout_For_WooCommerce_AJAX extends WC_AJAX {
 		}
 
 		WC()->session->set( 'chosen_shipping_methods', $chosen_shipping_methods );
-		
+
 		WC()->cart->calculate_shipping();
 		WC()->cart->calculate_fees();
 		WC()->cart->calculate_totals();
@@ -305,9 +305,10 @@ class Klarna_Checkout_For_WooCommerce_AJAX extends WC_AJAX {
 			wp_send_json_error( 'bad_nonce' );
 			exit;
 		}
-		$form = $_POST['form'];
-		set_transient( WC()->session->get( 'kco_wc_order_id' ), $form, 60 * 60 * 24 );
-
+		if ( isset( $_POST['form'] ) ) {
+			$form = $_POST['form'];
+			set_transient( WC()->session->get( 'kco_wc_order_id' ), $form, 60 * 60 * 24 );
+		}
 		wp_send_json_success();
 		wp_die();
 	}
