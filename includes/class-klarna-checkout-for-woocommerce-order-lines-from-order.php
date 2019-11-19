e5bc468911995d57885488a49d47e1e3b3761f70
MichaelBengtsson <michael@krokedil.se>
2019-11-19 08:57:45 +0100

    Order line from order coding standards

--- includes/class-klarna-checkout-for-woocommerce-order-lines-from-order.php
+++ includes/class-klarna-checkout-for-woocommerce-order-lines-from-order.php
@@ -1,19 +1,46 @@
 <?php
+/**
+ * Gets the order information from an order.
+ *
+ * @package Klarna_Checkout/Classes
+ */
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Class for processing order lines from a WooCommerce order.
+ */
 class Klarna_Checkout_For_WooCommerce_Order_Lines_From_Order {
+	/**
+	 * Gets the order amount
+	 *
+	 * @param int $order_id The WooCommerce order id.
+	 * @return int
+	 */
 	public function get_order_amount( $order_id ) {
 		$order = wc_get_order( $order_id );
 		return round( $order->get_total() * 100 );
 	}
 
+	/**
+	 * Get the order total tax.
+	 *
+	 * @param int $order_id The WooCommerce order id.
+	 * @return int
+	 */
 	public function get_total_tax( $order_id ) {
 		$order = wc_get_order( $order_id );
 		return round( $order->get_total_tax( $order_id ) * 100 );
 	}
 
+	/**
+	 * Gets the formated order line.
+	 *
+	 * @param WC_Order_Item_Product $order_item The WooCommerce order line item.
+	 * @return array
+	 */
 	public function get_order_line_items( $order_item ) {
 		$order_id = $order_item->get_order_id();
 		$order    = wc_get_order( $order_id );
@@ -27,6 +54,12 @@ class Klarna_Checkout_For_WooCommerce_Order_Lines_From_Order {
 		);
 	}
 
+	/**
+	 * Gets the formated order line shipping.
+	 *
+	 * @param WC_Order $order The WooCommerce order.
+	 * @return array
+	 */
 	public function get_order_line_shipping( $order ) {
 		return array(
 			'name'             => $order->get_shipping_method(),
@@ -38,6 +71,12 @@ class Klarna_Checkout_For_WooCommerce_Order_Lines_From_Order {
 		);
 	}
 
+	/**
+	 * Gets the formated order line fees.
+	 *
+	 * @param WC_Order_Item_Fee $order_fee The order item fee.
+	 * @return array
+	 */
 	public function get_order_line_fees( $order_fee ) {
 		$order_id = $order_fee->get_order_id();
 		$order    = wc_get_order( $order_id );
@@ -45,12 +84,19 @@ class Klarna_Checkout_For_WooCommerce_Order_Lines_From_Order {
 			'name'             => $order_fee->get_name(),
 			'quantity'         => $order_fee->get_quantity(),
 			'total_amount'     => round( ( $order_fee->get_total() + $order_fee->get_total_tax() ) * 100 ),
-			'unit_price'       => round( ( $order_fee->get_total() + $order_fee->get_total_tax() ) / $order_item->get_quantity() * 100 ),
+			'unit_price'       => round( ( $order_fee->get_total() + $order_fee->get_total_tax() ) * 100 ),
 			'total_tax_amount' => round( $order_fee->get_total_tax() * 100 ),
 			'tax_rate'         => ( '0' !== $order->get_total_tax() ) ? $this->get_order_line_tax_rate( $order, current( $order->get_items( 'fee' ) ) ) : 0,
 		);
 	}
 
+	/**
+	 * Gets the order line tax rate.
+	 *
+	 * @param WC_Order $order The WooCommerce order.
+	 * @param mixed    $order_item If not false the WooCommerce order item WC_Order_Item.
+	 * @return int
+	 */
 	public function get_order_line_tax_rate( $order, $order_item = false ) {
 		$tax_items = $order->get_items( 'tax' );
 		foreach ( $tax_items as $tax_item ) {
