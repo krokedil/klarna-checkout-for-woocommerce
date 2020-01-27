85c841b4fb21a3604f57c2104b2f06cab595ee61
Michael Bengtsson <michael@krokedil.se>
2020-01-27 14:51:04 +0100

    Changed from update klarna order to update_checkout event in force update

--- assets/js/klarna-checkout-for-woocommerce.js
+++ assets/js/klarna-checkout-for-woocommerce.js
@@ -40,9 +40,9 @@ jQuery(function($) {
 				kco_wc.paymentMethod = 'kco';
 			}
 
-			if( kco_params.force_update ) {
+			if( kco_params.force_update && 'kco' === kco_wc.paymentMethod ) {
 				console.log( 'force update' );
-				kco_wc.updateKlarnaOrder();
+				$('body').trigger('update_checkout');
 			}
 
 			kco_wc.confirmLoading();
