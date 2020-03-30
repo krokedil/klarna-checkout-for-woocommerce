bf0df14e2862154fe3cc91f9c6f0bf6b680eeae6
MichaelBengtsson <michael@krokedil.se>
2020-03-30 09:02:07 +0200

    Returns the region directly if its already an existing unicode format.

--- includes/kco-functions.php
+++ includes/kco-functions.php
@@ -540,7 +540,13 @@ function kco_confirm_klarna_order( $order_id = null, $klarna_order_id ) {
 function kco_convert_region( $region_string, $country_code ) {
 	$region_string = ucfirst( strtolower( $region_string ) );
 	$states        = include WC()->plugin_path() . '/i18n/states.php';
-	$region_code   = array_keys( $states[ strtoupper( $country_code ) ], $region_string, false );
+	// Check if the region is already unicode format.
+	if ( key_exists( strtoupper( $region_string ), $states[ strtoupper( $country_code ) ] ) ) {
+		return $region_string;
+	}
+
+	// Get the code by region name.
+	$region_code = array_keys( $states[ strtoupper( $country_code ) ], $region_string, false );
 
 	return $region_code[0];
 }
