ac9788fa31e802532774cccb2abcb6dcc93fea71
MichaelBengtsson <michael@krokedil.se>
2020-05-15 07:50:10 +0200

    Remove unused scripts for testing

--- tests/bin/phpunit.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [[ ${RUN_PHPCS} == 0 ]]; then
-	git clone --depth 1 --branch ${WC_VERSION} https://github.com/woocommerce/woocommerce.git '../woocommerce'
-	$HOME/.composer/vendor/bin/phpunit -c phpunit.xml $@
-fi
