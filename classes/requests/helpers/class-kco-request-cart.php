1ab34ef021a5d8629b6b3689b7e1a72e518291f3
MichaelBengtsson <michael@krokedil.se>
2020-10-07 09:54:24 +0200

    Fix dont include zero value lines as surcharge

--- classes/requests/helpers/class-kco-request-cart.php
+++ classes/requests/helpers/class-kco-request-cart.php
@@ -114,7 +114,7 @@ class KCO_Request_Cart {
 		$amount_to_adjust = $this->get_order_amount() - $this->get_order_lines_total_amount( $this->order_lines );
 
 		// If the amount to adjust is zero, return.
-		if ( 0 === $amount_to_adjust ) {
+		if ( 0 >= $amount_to_adjust ) {
 			return $this->order_lines;
 		}
 
