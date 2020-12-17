3d58dc52d43b0aa726aef18780ce85389f36324c
Kontrazen <krlovic@gmail.com>
2020-12-17 02:52:16 +0100

    Create expector functions

--- tests/e2e/helpers/kcoUtils.js
+++ tests/e2e/helpers/kcoUtils.js
@@ -24,7 +24,36 @@ const acceptTerms = async (page, selector) => {
 	await page.evaluate((cb) => cb.click(), await page.$(selector));
 };
 
+/**
+ * Check for selector - continue if false
+ */
+const expectSelector = async (frame, page, selector, time) => {
+	try {
+		await page.waitForTimeout(time);
+		await frame.waitForSelector(selector);
+
+		await frame.click(selector);
+	} catch {
+		console.log("Proceed from expectation");
+	}
+};
+
+/**
+ * Check for input - continue if false
+ */
+const expectInput = async (frame, page, inputValue, selector, time) => {
+	try {
+		await page.waitForTimeout(time);
+		await frame.waitForSelector(selector);
+		await frame.type(selector, inputValue);
+	} catch {
+		console.log("Proceed from expectation");
+	}
+};
+
 export default {
 	acceptTerms,
 	selectPayment,
+	expectSelector,
+	expectInput,
 };
