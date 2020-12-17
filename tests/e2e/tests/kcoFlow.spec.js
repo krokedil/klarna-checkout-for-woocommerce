df1d3902cb08a81216da810d57334c45c52fd1ba
Kontrazen <krlovic@gmail.com>
2020-12-17 13:33:45 +0100

    Update with potential selectors

--- tests/e2e/tests/kcoFlow.spec.js
+++ tests/e2e/tests/kcoFlow.spec.js
@@ -13,7 +13,9 @@ import {
 	freeShippingMethodTarget,
 	flatRateMethod,
 	flatRateMethodTarget,
-    creditPaymentMethod,
+	creditPaymentMethod,
+	debitPaymentMethod,
+	invoicePaymentMethod,
 	billingData,
 	userCredentials,
 	timeOutTime,
@@ -41,7 +43,7 @@ if (shippingMethod === "free") {
 /**
  * Payment Method
  */
-const selectedPaymentMethod = creditPaymentMethod;
+const selectedPaymentMethod = invoicePaymentMethod;
 
 describe("KCO", () => {
 	beforeAll(async () => {
@@ -88,108 +90,59 @@ describe("KCO", () => {
 		);
 
 		if (shippingMethod !== "") {
-			await page.waitForTimeout(2 * timeOutTime);
+			await page.waitForTimeout(timeOutTime);
 			await page.waitForSelector(shippingMethodTarget).id;
 			await page.waitForTimeout(timeOutTime);
 			await page.click(shippingMethodTarget).id;
 			await page.waitForTimeout(timeOutTime);
 		}
 
-		const originalFrame = await kcoFrame.loadIFrame(
-			page,
-			"klarna-checkout-iframe"
-        );
-
+		const originalFrame = await kcoFrame.loadIFrame(page,"klarna-checkout-iframe");
 		kcoFrame.submitBillingForm(originalFrame, billingData);
 
-		const frameNew = await kcoFrame.loadIFrame(
-			page,
-			"klarna-fullscreen-iframe"
-		);
 
 		await page.waitForTimeout(timeOutTime);
-		await kcoUtils.expectSelector(
-			originalFrame,
-			page,
-			'[data-cid="button.buy_button"]',
-			timeOutTime
-		);
+		await kcoUtils.expectSelector(originalFrame, page, '[data-cid="am.continue_button"]', timeOutTime)
+
+
+		await kcoUtils.expectSelector(originalFrame, page, 'input[id="payment-selector-pay_now"]', timeOutTime)
+
+		const frameNew = await kcoFrame.loadIFrame(page,"klarna-fullscreen-iframe");
+
+		await page.waitForTimeout(timeOutTime);
+		await kcoUtils.expectSelector(originalFrame,page,'[data-cid="button.buy_button"]',timeOutTime);
 
 		if (selectedPaymentMethod === "credit") {
-			await kcoUtils.expectSelector(
-				frameNew,
-				page,
-				'div[id*=".paynow_card."]',
-				timeOutTime
-			);
+			await kcoUtils.expectSelector(frameNew,	page,'div[id*=".paynow_card."]',timeOutTime);
 
-			const frameCreditCard = await kcoFrame.loadIFrame(
-				page,
-				"pgw-iframe-paynow_card"
-			);
-			await kcoUtils.expectInput(
-				frameCreditCard,
-				page,
-				cardNumber,
-				'input[id="cardNumber"]',
-				0.25 * timeOutTime
-			);
-			await kcoUtils.expectInput(
-				frameCreditCard,
-				page,
-				"1122",
-				'input[id="expire"]',
-				0.25 * timeOutTime
-			);
-			await kcoUtils.expectInput(
-				frameCreditCard,
-				page,
-				"123",
-				'input[id="securityCode"]',
-				0.25 * timeOutTime
-			);
+			const frameCreditCard = await kcoFrame.loadIFrame(page,"pgw-iframe-paynow_card");
+			await kcoUtils.expectInput(	frameCreditCard,page,cardNumber,'input[id="cardNumber"]',0.25 * timeOutTime);
+			await kcoUtils.expectInput(frameCreditCard,page,"1122",'input[id="expire"]',0.25 * timeOutTime);
+			await kcoUtils.expectInput(	frameCreditCard,page,"123",	'input[id="securityCode"]',	0.25 * timeOutTime);
 		} else if (selectedPaymentMethod === "debit") {
-			await kcoUtils.expectSelector(
-				frameNew,
-				page,
-				'[data-cid="payment-selector-method.direct_debit"]',
-				timeOutTime
+			await kcoUtils.expectSelector(frameNew,	page,'[data-cid="payment-selector-method.direct_debit"]',timeOutTime
 			);
 		} else if (selectedPaymentMethod === "invoice") {
-			await kcoUtils.expectSelector(
-				frameNew,
-				page,
-				'input[id*=".invoice."]',
-				timeOutTime
-			);
+			await kcoUtils.expectSelector(frameNew,	page,'input[id*=".invoice."]',timeOutTime);
 		}
 
-		await kcoUtils.expectSelector(
-			frameNew,
-			page,
-			'[data-cid="button.buy_button"]',
-			timeOutTime
-		);
+		await kcoUtils.expectSelector(frameNew,	page,'[data-cid="button.buy_button"]',timeOutTime);
+		await kcoUtils.expectSelector(frameNew,page,'button[data-cid="skip-favorite-dialog-confirm-button"]',timeOutTime);
 
-		await kcoUtils.expectSelector(
-			frameNew,
-			page,
-			'button[data-cid="skip-favorite-dialog-confirm-button"]',
-			timeOutTime
-		);
+		await kcoUtils.expectSelector(frameNew, page, '[id="nin"]', timeOutTime);
+		await kcoUtils.expectInput(frameNew, page, pinNumber, '[id="nin"]', timeOutTime);
+		await kcoUtils.expectSelector(frameNew, page, '[id="supplement_nin_dialog__footer-button-wrapper"]', timeOutTime);
 
+		await page.waitForTimeout(2 * timeOutTime);
+		await kcoUtils.expectSelector(page.frames().find(fr => fr.name() === 'klarna-fullscreen-iframe'), page, '[id="confirm_bank_account_dialog__footer-button-wrapper"]', page, '[id="confirm_bank_account_dialog__footer-button-wrapper"]', timeOutTime);
+		await page.waitForTimeout(2 * timeOutTime);
 
-
-		await page.waitForTimeout(10 * timeOutTime);
+		await page.waitForTimeout(5 * timeOutTime);
 		const currentURL = await page.url();
 		const currentKCOId = currentURL.split("kco_order_id=")[1];
-		const KCOResponse = await API.getKlarnaOrderById(
-			page,
-			klarnaOrderEndpoint,
-			currentKCOId
-		);
+		const KCOResponse = await API.getKlarnaOrderById(page,klarnaOrderEndpoint,currentKCOId);
 
-		await page.waitForTimeout(4 * timeOutTime);
+		await page.waitForTimeout(3 * timeOutTime);
 		const value = await page.$eval(".entry-title", (e) => e.textContent);
 		await page.screenshot({ path: "./order", type: "png" });
 
