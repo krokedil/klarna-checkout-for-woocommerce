603fc981a73cf6e0f7ff4c4f138220af83cca6d8
Miodrag Stojanovic <mio.sto@yandex.com>
2020-12-18 20:26:35 +0100

    add shorthand for variable

--- tests/e2e/config/config.js
+++ tests/e2e/config/config.js
@@ -2,7 +2,7 @@ import config from "./config.data.json";
 
 export const adminData = config?.users?.admin;
 export const customerData = config.users.customer;
-
+export const shippingTargets = customerData.shipping.targets;
 export const customerKey = config.users.customer.api.consumerKey;
 export const customerSecret = config.users.customer.api.consumerSecret;
 export const klarnaAuth = config.users.customer.klarnaCredentials;
