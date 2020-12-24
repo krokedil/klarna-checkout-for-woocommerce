a0858848f07b39b7c0c6740a5ce4a8151ee03bd9
Kontrazen <krlovic@gmail.com>
2020-12-24 14:19:11 +0100

    Fix helper functions

--- tests/e2e/helpers/kcoCart.js
+++ tests/e2e/helpers/kcoCart.js
@@ -1,8 +1,4 @@
 /**
- * Helper functions for adding a product to the cart.
- */
-
-/**
  * Adds one product to the cart.
  *
  * @param page
@@ -10,13 +6,14 @@
  * @returns {Promise<void>}
  */
 const addSingleProductToCart = async (page, productId) => {
-	const productSelector = `a[href="?add-to-cart=${productId}"]`;
-	await page.waitForSelector(productSelector);
-	await page.evaluate(
-		(selector) => document.querySelector(selector).click(),
-		productSelector
-	);
+	let productSelector = `a[href="?add-to-cart=${productId}"]`;
+	try {
+		await page.click(productSelector);
+	} catch {
+		console.log("Proceed from expectation");
+	}
 };
+
 
 /**
  * Adds multiple products to the cart.
@@ -26,9 +23,14 @@ const addSingleProductToCart = async (page, productId) => {
  * @returns {Promise<void>}
  */
 const addMultipleProductsToCart = async (page, products) =>
-	products.map(async (productId) => {
-		await addSingleProductToCart(page, productId);
-	});
+	{
+		products.forEach(async product => {
+			await addSingleProductToCart(page, product)
+		})
+		await page.waitForTimeout(2000);
+	}
+
+
 
 export default {
 	addSingleProductToCart,
