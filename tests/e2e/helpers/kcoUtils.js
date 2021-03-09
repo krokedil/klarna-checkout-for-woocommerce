71d186da6c96248eb55dc51cc13fe2f0d620bc23
Kontrazen <krlovic@gmail.com>
2021-03-09 13:47:17 +0100

    CCreate add coupon function

--- tests/e2e/helpers/kcoUtils.js
+++ tests/e2e/helpers/kcoUtils.js
@@ -32,7 +32,6 @@ const expectSelector = async (frame, page, selector, time) => {
 		await page.waitForTimeout(2 * time);
 		await frame.click(selector);
 	} catch {
-		console.log("Proceed from expectation");
 	}
 };
 
@@ -44,13 +43,38 @@ const expectInput = async (frame, page, inputValue, selector, time) => {
 		await page.waitForTimeout(2 * time);
 		await frame.type(selector, inputValue);
 	} catch {
-		console.log("Proceed from expectation");
 	}
 };
 
+/**
+ * Add coupons
+ */
+const addCouponsOnCheckout = async (page, appliedCoupons) => {
+	await page.waitForTimeout(1000);
+	await page.$eval('[id="wpadminbar"]', (adminBar) =>
+		adminBar.setAttribute("style", "display:none")
+	);
+
+	if (appliedCoupons.length > 0) {
+		await appliedCoupons.forEach(async (singleCoupon) => {
+			await page.waitForTimeout(1000);
+			await page.click('[class="showcoupon"]');
+			await page.waitForTimeout(1000);
+			await page.type('[name="coupon_code"]', singleCoupon);
+			await page.waitForTimeout(1000);
+			await page.click('[name="apply_coupon"]');
+			await page.waitForTimeout(1000);
+		});
+	}
+};
+
+/**
+ * Export data
+ */
 export default {
 	acceptTerms,
 	selectPayment,
 	expectSelector,
 	expectInput,
+	addCouponsOnCheckout,
 };
