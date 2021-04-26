7f71f7c17a17c1f2b8f1b3c26df5a1d70313395e
Miodrag Stojanovic <mio.sto@yandex.com>
2021-04-26 12:59:03 +0200

    remove sh scripts

--- tests/e2e/bin/import.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p ./wp-content/uploads/2020/12
-cp -R ./wp-content/plugins/klarna-checkout-for-woocommerce/tests/e2e/data/imgs/10/* ./wp-content/uploads/2020/12/
-ls -la ./wp-content/uploads/2020/12/
-
-
-wp db import ./wp-content/plugins/klarna-checkout-for-woocommerce/tests/e2e/bin/data.sql
-
-wp import ./wp-content/plugins/klarna-checkout-for-woocommerce/tests/e2e/bin/coupons.xml --authors=create;
