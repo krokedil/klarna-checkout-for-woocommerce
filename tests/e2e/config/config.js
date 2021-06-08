41bcdb69aaf6dd58c55f7ed2eb4d67f12db7664b
Kontrazen <krlovic@gmail.com>
2021-06-08 11:50:45 +0200

    Update product variables

--- tests/e2e/config/config.js
+++ tests/e2e/config/config.js
@@ -68,42 +68,19 @@ export const customerLastname = customerData.last_name;
 export const customerEmail = customerData.email;
 export const customerUsername = customerData.username;
 
-export const simpleProduct25 = productIdData.simple_25;
-export const simpleProduct12 = productIdData.simple_12;
-export const simpleProduct6 = productIdData.simple_6;
-export const simpleProduct0 = productIdData.simple_0;
-export const simpleProductSale25 = productIdData.simple_sale_25;
-export const simpleProductSale12 = productIdData.simple_sale_12;
-export const simpleProductSale6 = productIdData.simple_sale_6;
-export const simpleProductSale0 = productIdData.simple_sale_0;
-export const variableProduct25Black = productIdData.variable_25_black;
-export const variableProduct25Blue = productIdData.variable_25_blue;
-export const variableProduct25Brown = productIdData.variable_25_brown;
-export const variableProduct25Green = productIdData.variable_25_green;
-export const variableProductMixedBlackS = productIdData.variable_mix_black_s;
-export const variableProductMixedBlackM = productIdData.variable_mix_black_m;
-export const variableProductMixedBlackL = productIdData.variable_mix_black_l;
-export const variableProductMixedBlackXL = productIdData.variable_mix_black_xl;
-export const variableProductMixedGreenS = productIdData.variable_mix_green_s;
-export const variableProductMixedGreenM = productIdData.variable_mix_green_m;
-export const variableProductMixedGreenL = productIdData.variable_mix_green_l;
-export const variableProductMixedGreenXL = productIdData.variable_mix_green_xl;
-export const variableProductVirtualDownloadable25 =
-	productIdData.virtual_downloadable_25;
-export const variableProductVirtualDownloadable12 =
-	productIdData.virtual_downloadable_12;
-export const variableProductVirtualDownloadable6 =
-	productIdData.virtual_downloadable_6;
-export const variableProductVirtualDownloadable0 =
-	productIdData.virtual_downloadable_0;
-export const variableProductVirtualDownloadableSale25 =
-	productIdData.virtual_downloadable_sale_25;
-export const variableProductVirtualDownloadableSale12 =
-	productIdData.virtual_downloadable_sale_12;
-export const variableProductVirtualDownloadableSale6 =
-	productIdData.virtual_downloadable_sale_6;
-export const variableProductVirtualDownloadableSale0 =
-	productIdData.virtual_downloadable_0;
+export const outOfStock = productIdData.out_of_stock;
+export const variable25 = productIdData.variable_25
+export const downloadable0 = productIdData.downloadable_0
+export const downloadable25 = productIdData.downloadable_25
+export const downloadableShipping0 = productIdData.downloadable_shipping_0
+export const downloadableShipping25 = productIdData.downloadable_shipping_25
+export const simple12 = productIdData.simple_12
+export const simple6 = productIdData.simple_6
+export const virtual0 = productIdData.virtual_0
+export const virtual25 = productIdData.virtual_25
+export const virtualDownloadable0 = productIdData.virtual_downloadable_0
+export const virtualDownloadable25 = productIdData.virtual_downloadable_25
+export const manyCharacters = productIdData.many_characters
 
 export const couponFixedCart = customerData.coupons.fixed_cart;
 export const couponFixedProduct = customerData.coupons.fixed_product;
