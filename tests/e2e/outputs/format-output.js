e6dfe6e7bb0e8262b66023d583bca17186720261
Kontrazen <krlovic@gmail.com>
2022-11-21 13:51:47 +0100

    Add test variable case

--- tests/e2e/outputs/format-output.js
+++ tests/e2e/outputs/format-output.js
@@ -12,7 +12,16 @@ const { SITEHOST, PORT, PLUGIN_NAME, KOM } = process.env;
 //     stdio: "inherit",
 // });
 
-let xString = '{ "test1": "' + process.env.PLUGIN_NAME + '"}'
+// let xString = '{ "test1": "' + process.env.PLUGIN_NAME + '"}'
 
-console.log(process.env.PLUGIN_NAME)
+let xString = `
+{
+    "wc_version": "${process.env.WC_VERSION}",
+    "wp_version": "${process.env.WP_VERSION}",
+    "plugin_name": "${process.env.PLUGIN_NAME}",
+    "branch_name": "krokedil-dev-test",
+    "link_to_test": "http://teste2elink.e2e.com"
+}`
+
+console.log(process.env)
 
