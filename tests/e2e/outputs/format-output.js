cc5ebb5e1bbf6112910f83b9edeb2d2c5b2a4643
Kontrazen <krlovic@gmail.com>
2022-11-21 13:40:02 +0100

    Add test variable case

--- tests/e2e/outputs/format-output.js
+++ tests/e2e/outputs/format-output.js
@@ -1,24 +1,16 @@
 require("dotenv").config();
+
+const { execSync } = require("child_process");
+
 const { SITEHOST, PORT, PLUGIN_NAME, KOM } = process.env;
-const  data  = require('./output.json');
-// import { data } from './output.json'
+// const  data  = require('./output.json');
+// // import { data } from './output.json'
 
-let newData = JSON.stringify(data)
+// let newData = JSON.stringify(data)
 
-let newRow = newData.split(',')
-let x = []
-
-newRow.forEach(element => {
-    element = element.replace(/\"/g, '')
-    if(element[0] == "{") {
-        element = element.replace(element[0], '')
-    }
-
-    x.push(element)
-});
-
-x = x.toString()
-x = x.replace(/.$/, '')
+// execSync(`php -version`, {
+//     stdio: "inherit",
+// });
 
 let xString = '{ "test1": "' + process.env.PLUGIN_NAME + '"}'
 
