000167b1510908d9f78a068b845056ad6634041a
Kontrazen <krlovic@gmail.com>
2022-11-23 16:50:49 +0100

    Obtain test versions

--- tests/e2e/helpers/formatOutput.js
+++ tests/e2e/helpers/formatOutput.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
 
-let formattedOutput = '{ "wc_version": "' + process.env.WC_VERSION + '", "wp_version": "' + process.env.WP_VERSION + '", "plugin_name": "' + process.env.PLUGIN_NAME + '" }'
+let formattedOutput = '{ "wc_version": "' + process.env.WC_VERSION + ', "plugin_name": "' + process.env.PLUGIN_NAME + '" }'
 
 console.log(formattedOutput)
