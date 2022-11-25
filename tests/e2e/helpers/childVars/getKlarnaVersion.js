86476120dea5607f23af1204c16a8a88b2bcfe2f
Kontrazen <krlovic@gmail.com>
2022-11-25 15:16:47 +0100

    Obtain test versions

---
+++ tests/e2e/helpers/childVars/getKlarnaVersion.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+const { execSync } = require("child_process");
+
+execSync("docker-compose run --rm wordpress-cli wp plugin get klarna-checkout-for-woocommerce --format=yaml", {
+    stdio: "inherit",
+});
--- tests/e2e/helpers/childVars/getWcVersion.js
+++ tests/e2e/helpers/childVars/getWcVersion.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const { execSync } = require("child_process");
 
-execSync("docker-compose run --rm wordpress-cli wp plugin get klarna-checkout-for-woocommerce --format=yaml", {
-// execSync("docker-compose run --rm wordpress-cli wp plugin get woocommerce --format=yaml", {
+execSync("docker-compose run --rm wordpress-cli wp plugin get woocommerce --format=yaml", {
     stdio: "inherit",
 });
