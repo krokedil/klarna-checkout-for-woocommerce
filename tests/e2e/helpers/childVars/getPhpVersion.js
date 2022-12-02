799b9515f57b6e8149f537c67956637d7b9aa168
Kontrazen <krlovic@gmail.com>
2022-12-02 14:34:27 +0100

    Update .env to PHP 8.1

--- tests/e2e/helpers/childVars/getPhpVersion.js
+++ tests/e2e/helpers/childVars/getPhpVersion.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 const { execSync } = require("child_process");
 
-execSync("docker-compose run --rm wordpress-cli wp cli info", {
+execSync("docker exec e2e_wordpress-dev_1 php --version", {
     stdio: "inherit",
 });
