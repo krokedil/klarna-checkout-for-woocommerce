496e1473da2bd2272857b500c0d030a45694507d
Montazar Al-Jaber <github.v0its@slmail.me>
2023-01-29 19:41:25 +0100

    Add settings, support and add-on tabs

--- classes/admin/class-wc-klarna-banners.php
+++ classes/admin/class-wc-klarna-banners.php
@@ -151,14 +151,141 @@ if ( ! class_exists( 'WC_Klarna_Banners' ) ) {
 			?>
 			<img id="klarna-settings-logo"
 				src="<?php echo esc_url( KCO_WC_PLUGIN_URL ); ?>/assets/img/klarna_logo_black.png" width="200"/>
+			
+			<style>
+				#kco-support, #kco-addons {
+					display: none;
+				}
+
+				.kco-tab-wrapper {
+					display: flex;
+					justify-content: space-between;
+					align-items: center;
+				}
+			</style>
+
+			<script>
+				jQuery(document).ready(function($){
+					$('#klarna-wrapper #tabs a').click(function(){
+						$('#klarna-wrapper #tabs a').removeClass('nav-tab-active');
+						$(this).addClass('nav-tab-active');
+						$(this).siblings().each(function(){
+							$('#' + $(this).attr('href').replace('#', '')).hide();
+						})
+						$('#' + $(this).attr('href').replace('#', '')).show();
+					});
+				});
+			</script>
 
 			<div id="klarna-wrapper">
-				<div id="klarna-main">
-					<?php echo $parent_options; // phpcs:ignore?>
+				<div id='tabs' class='nav-tab-wrapper'>
+					<nav>
+						<a class='nav-tab nav-tab-active' href="#kco-settings">Settings</a>
+						<a class='nav-tab' href="#kco-support">Support</a>
+						<a class='nav-tab' href="#kco-addons">Add-ons</a>
+					</nav>
+				</div>
+				<div class='kco-tab-wrapper'>
+				<div id='kco-settings'>
+					<div id="klarna-main">
+						<?php echo $parent_options; // phpcs:ignore?>
+					</div>
+				</div>
+				<div id='kco-support'>
+					<p>Before opening a support ticket, please make sure you have read the relevant plugin resources for a solution to your problem.</p>
+					<ul>
+						<li><a href="https://krokedil.com/product/klarna-checkout-for-woocommerce/" target="_blank">General information</a></li>
+						<li><a href="https://docs.krokedil.com/klarna-checkout-for-woocommerce/" target="_blank">Technical documentation</a></li>
+						<li><a href="https://docs.krokedil.com/krokedil-general-support-info/" target="_blank">General support information</a></li>
+					</ul>
+					<p>If you have questions regarding a certain purchase, you're welcome to contact <a href="">Klarna.</a></p>
+					<p>If you have <b>technical questions or questions regarding configuration</b> of the plugin, you're welcome to contact <a href="">Krokedil</a>, the plugin's developer.</p>
+				</div>
+				<style>
+					.kco-addons-cards {
+						display: flex;
+						flex-wrap: wrap;
+						justify-content: space-between;
+						margin-bottom: 1.2em;
+						max-width: 1080px;
+					}
+					.kco-addon-card {
+						display: flex;
+						justify-content: space-between;
+						flex-direction: column;
+						width: 30%;
+						border: 1px solid #ccc;
+						border-radius: 3px;
+						background-color: white;
+					}
+
+					.kco-addon-card a[target="_blank"]::after {
+						content: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQElEQVR42qXKwQkAIAxDUUdxtO6/RBQkQZvSi8I/pL4BoGw/XPkh4XigPmsUgh0626AjRsgxHTkUThsG2T/sIlzdTsp52kSS1wAAAABJRU5ErkJggg==);
+						margin: 0 3px 0 5px;
+					}
+
+					.kco-addon-card-image {
+						width: 100%;
+						background-size: cover;
+						background-position: center;
+						border-radius: 3px 3px 0 0;
+					}
+					.kco-addon-card-title {
+						font-weight: bold;
+						padding: 0 0.7em;
+					}
+					.kco-addon-card-description {
+						padding: 1.1em;
+						padding-top: 0;
+					}
+
+					.kco-addon-read-more {
+						display: block;
+						padding: 1.1em;
+						padding-top: 0;
+						border-bottom: 2px solid lightgray;
+					}
+
+					.kco-addon-card-action {
+						display: flex;
+						justify-content: space-between;
+						align-items: center;
+						padding: 0 1.1em;
+						font-weight: bold;
+					}
+				</style>
+				<div id='kco-addons'>
+					<p>These are other plugins from Krokedil that work well with the plugin Klarna Checkout.</p>
+					<div class='kco-addons-cards'>
+						<div class="kco-addon-card">
+							<img class="kco-addon-card-image" src="https://krokedil.com/wp-content/uploads/sites/3/2020/11/kom-chosen-960x544.jpg" alt="Klarna Order Management logo">
+							<h3 class="kco-addon-card-title">Klarna Order Management</h3>
+							<p class="kco-addon-card-description">Handle post purchase order management in Klarna's system directly from WooCommerce. This way you can save time and don't have to work in both systems simultaneously.</p>
+							<a class="kco-addon-read-more" href="https://krokedil.com/product/klarna-order-management/" target="_blank">Read more</a>
+							<p class="kco-addon-card-action"><span class='kco-addon-card-price'>Free</span><a class="install-now button" href="https://krokedil.com/product/krokedil-general-support-info/" target="_blank">Install now</a></p>
+						</div>
+
+
+						<div class="kco-addon-card">
+							<img class="kco-addon-card-image" src="https://krokedil.com/wp-content/uploads/sites/3/2020/11/osm-chosen-960x544.jpg" alt="Krokedil logo">
+							<h3 class="kco-addon-card-title">On-Site Messaging</h3>
+							<p class="kco-addon-card-description">On-Site Messaging is easy and simple to integrate, providing tailored messaging ranging from generic banners to promote your partnership with Klarna and availability of financing to personalized credit promotion on product or cart pages.</p>
+							<a class="kco-addon-read-more" href="https://krokedil.com/product/on-site-messaging-for-woocommerce/" target="_blank">Read more</a>
+							<p class="kco-addon-card-action"><span class='kco-addon-card-price'>Free</span><a class="install-now button"  href="https://krokedil.com/product/klarna-payments-for-woocommerce/" target="_blank">Install now</a></p>
+						</div>
+						
+						<div class="kco-addon-card">
+							<img class="kco-addon-card-image" src="https://krokedil.com/wp-content/uploads/sites/3/2021/08/featured-partial-delivery-960x544.jpg" alt="Partial Delivery for WooCommerce logo">
+							<h3 class="kco-addon-card-title">Partial Delivery for WooCommerce</h3>
+							<p class="kco-addon-card-description">Partial Delivery for WooCommerce enables merchants to ship parts of an order. Via a logical interface you are able to send one or several items in each delivery.</p>
+							<a class="kco-addon-read-more" href="https://krokedil.com/product/partial-delivery-for-woocommerce/" target="_blank">Read more</a>
+							<p class="kco-addon-card-action"><span class='kco-addon-card-price'>250 SEK/month</span><a class="install-now button" href="https://krokedil.com/product/klarna-checkout-for-woocommerce/" target="_blank">Buy Now</a></p>
+						</div>
+						
+					</div>
 				</div>
 
 				<div id="krokdocs-sidebar">
-
 					<div class="krokdocs-sidebar-section">
 						<h1 id="krokdocs-sidebar-title">Plugin resources</h1>
 							<div class="krokdocs-sidebar-content">
@@ -184,6 +311,7 @@ if ( ! class_exists( 'WC_Klarna_Banners' ) ) {
 									src="https://krokedil.se/wp-content/uploads/2020/05/webb_logo_400px.png">
 								</div>
 					</div>
+				</div>
 				</div>
 			<div class="save-separator"></div>
 			<?php
