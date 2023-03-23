53500ac9a22a3ece867138ce673cf430bb83a9cb
Montazar Al-Jaber <github.v0its@slmail.me>
2023-03-23 03:18:26 +0100

    Process support form for submission to support

--- classes/class-krokedil-support-form.php
+++ classes/class-krokedil-support-form.php
@@ -11,17 +11,46 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Krokedil_Support_Form {
 
-	private static $init = false;
+	private static $init    = false;
+	private static $version = '1.0.0';
 
 	public static function init() {
-		if ( self::$init ) {
-			return;
-		}
+		// if ( self::$init ) {
+		// return;
+		// }
 
+		add_action( 'admin_init', array( __CLASS__, 'process_support_form' ) );
 		add_action( 'wc_ajax_krokedil_set_plugin_status', array( __CLASS__, 'set_plugin_status' ) );
+		add_action( 'wc_ajax_krokedil_get_plugin_log_content', array( __CLASS__, 'get_plugin_log_content' ) );
+		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'enqueue_assets' ) );
 
 		self::$init = true;
 	}
+
+	public static function enqueue_assets() {
+		wp_enqueue_style(
+			'krokedil_support_form_styles',
+			plugins_url( 'assets/css/krokedil-support-form.css', KCO_WC_MAIN_FILE ),
+			array(),
+			self::$version
+		);
+
+		wp_register_script(
+			'krokedil_support_form_script',
+			plugins_url( 'assets/js/krokedil-support-form.js', KCO_WC_MAIN_FILE ),
+			array(),
+			self::$version,
+			true
+		);
+		$admin_localize_params = array(
+			'get_plugin_log_content_url'   => WC_AJAX::get_endpoint( 'krokedil_get_plugin_log_content' ),
+			'get_plugin_log_content_nonce' => wp_create_nonce( 'krokedil_get_plugin_log_content' ),
+		);
+		wp_localize_script( 'krokedil_support_form_script', 'krokedil_support_form_params', $admin_localize_params );
+		wp_enqueue_script( 'krokedil_support_form_script' );
+
+	}
+
 
 	/**
 	 * Check if a plugin is activated.
@@ -38,7 +67,7 @@ class Krokedil_Support_Form {
 	}
 
 	/**
-	 * Get the plugin's log files.
+	 * Get all the log filenames that are related to a plugin.
 	 *
 	 * @param string $plugin_name The plugin's directory name, and main plugin filename. Defaults to 'all'.
 	 * @return array
@@ -61,6 +90,58 @@ class Krokedil_Support_Form {
 		}
 
 		return $logs;
+	}
+
+	/**
+	 * Get the content of a WooCommerce log file.
+	 *
+	 * @return void The log content is sent back in the JSON response.
+	 */
+	public static function get_plugin_log_content() {
+		$nonce = isset( $_GET['nonce'] ) ? sanitize_key( $_GET['nonce'] ) : '';
+		if ( ! wp_verify_nonce( $nonce, 'krokedil_get_plugin_log_content' ) ) {
+			wp_send_json_error( 'bad_nonce' );
+		}
+
+		/* Check if the user can access WC logs. */
+		if ( ! current_user_can( 'manage_woocommerce' ) ) {
+			wp_send_json_error( 'bad_permissions' );
+		}
+
+		$filename = isset( $_GET['filename'] ) ? sanitize_text_field( wp_unslash( $_GET['filename'] ) ) : '';
+		$content  = file_get_contents( WC_LOG_DIR . $filename );
+		wp_send_json_success( $content );
+	}
+
+	/**
+	 * Outputs the HTML for selecting a WooCommerce log, and displaying its content.
+	 *
+	 * @param string $title The heading for this HTML section.
+	 * @param array  $log An array containing the log content.
+	 * @return void The HTML is output directly.
+	 */
+	public static function output_log_selector( $title, $log ) {
+
+		// Normalized name intended to be form and CSS friendly.
+		$name = esc_attr( str_replace( ' ', '-', strtolower( $title ) ) );
+		?>
+	<section class="log-wrapper <?php esc_attr_e( $name ); ?>">
+		<h3><?php echo $title; ?></h3>
+		<?php
+		if ( empty( $log ) ) {
+			echo '<p class="' . $name . '">No log available.</p>';
+			return;
+		}
+		?>
+		<select class="kco-log-option wc-enhanced-select <?php echo $name; ?> " name="<?php echo $name; ?>">
+			<?php foreach ( $log as $date => $path ) : ?>
+			<option value="<?php esc_attr_e( $path ); ?>"><?php echo esc_html( "{$path} ({$date})" ); ?></option>
+			<?php endforeach; ?>
+		</select>
+		<a class="view-log">View log</a>
+		<textarea class="log-content" readonly></textarea>
+	</section>
+		<?php
 	}
 
 	/**
@@ -162,6 +243,83 @@ class Krokedil_Support_Form {
 			}
 		}
 
+	}
+
+	/**
+	 * Process the form for submission to support.
+	 *
+	 * @return void
+	 */
+	public static function process_support_form() {
+		if ( isset( $_POST['submit'] ) ) {
+			if ( ! current_user_can( 'manage_options' ) ) {
+				return;
+			}
+
+			if ( ! isset( $_POST['krokedil_support_form_nonce'] ) || ! wp_verify_nonce( $_POST['krokedil_support_form_nonce'], 'krokedil_support_form_nonce' ) ) {
+				return;
+			}
+
+			$error = false;
+			if ( ! is_email( sanitize_email( $_POST['email'] ) ) ) {
+				$error = true;
+			}
+
+			$from    = sanitize_email( $_POST['email'] );
+			$to      = 'support@krokedil.se';
+			$subject = sanitize_text_field( $_POST['subject'] );
+			$message = sanitize_textarea_field( $_POST['description'] );
+			$headers = array(
+				'From: ' . $from,
+				'Reply-To: ' . $from,
+				'Content-Type: text/html; charset=UTF-8',
+			);
+
+			$attachment = array();
+
+			if ( isset( $_POST['klarna-checkout'] ) ) {
+				$attachment[] = WC_LOG_DIR . sanitize_text_field( $_POST['klarna-checkout'] );
+			}
+
+			if ( isset( $_POST['klarna-order-management'] ) ) {
+				$attachment[] = WC_LOG_DIR . sanitize_text_field( $_POST['klarna-order-management'] );
+			}
+
+			if ( isset( $_POST['fatal-error-log'] ) ) {
+				$attachment[] = WC_LOG_DIR . sanitize_text_field( $_POST['fatal-error-log'] );
+			}
+
+			if ( isset( $_POST['additional-log'] ) ) {
+				$attachment[] = WC_LOG_DIR . sanitize_text_field( $_POST['additional-log'] );
+			}
+
+			foreach ( $_POST as $key => $value ) {
+				if ( strpos( $key, 'additional-log-' ) !== false ) {
+					$attachment[] = WC_LOG_DIR . sanitize_text_field( $value );
+				}
+			}
+
+			$attachment = array_filter(
+				$attachment,
+				function( $path ) {
+					return file_exists( $path ) && is_readable( $path );
+				},
+			);
+
+			if ( 'on' === $_POST['system-report-include'] ) {
+				$system_report = sanitize_textarea_field( $_POST['system-report'] );
+				$filename      = 'system-report-' . date( 'Y-m-d' ) . '.txt';
+				$filepath      = WP_CONTENT_DIR . '/uploads/' . $filename;
+
+				if ( file_put_contents( $filepath, $system_report ) ) {
+					$attachment[] = $filepath;
+				}
+			}
+
+			if ( ! $error ) {
+				$error = wp_mail( $to, $subject, $message, $headers, $attachment );
+			}
+		}
 	}
 
 }
--- includes/admin/views/html-kco-support.php
+++ includes/admin/views/html-kco-support.php
@@ -9,13 +9,25 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+$logs = array(
+	'kco'   => Krokedil_Support_Form::get_plugin_logs( 'klarna-checkout-for-woocommerce' ),
+	'kom'   => Krokedil_Support_Form::get_plugin_logs( 'klarna-order-management-for-woocommerce' ),
+	'fatal' => Krokedil_Support_Form::get_plugin_logs( 'fatal-errors' ),
+	'all'   => Krokedil_Support_Form::get_plugin_logs(),
+);
+
 // Hides the WooCommerce save button for the settings page.
 $GLOBALS['hide_save_button'] = true;
 
+
+$system_report = new WC_Admin_Status();
 ?>
+<div style="position: absolute; top: -9999px; left: -9999px;">
+	<?php echo $system_report->status_report(); ?>
+	<input type="hidden" name="system-report" id="system-report">
+</div>
 <div id='kco-support'>
-	<p>Before opening a support ticket, please make sure you have read the relevant plugin resources for a solution to
-		your problem:</p>
+	<p>Before opening a support ticket, please make sure you have read the relevant plugin resources for a solution to your problem:</p>
 	<ul>
 		<li><a href="<?php echo $links['General information']['href']; ?>" target="_blank">General information</a></li>
 		<li><a href="<?php echo $links['Technical documentation']['href']; ?>" target="_blank">Technical documentation</a></li>
@@ -23,4 +35,61 @@ $GLOBALS['hide_save_button'] = true;
 	</ul>
 	<p>If you have questions regarding a certain purchase, you're welcome to contact <a href="<?php echo $links['Klarna']['href']; ?>" target="_blank">Klarna.</a></p>
 	<p>If you have <b>technical questions or questions regarding configuration</b> of the plugin, you're welcome to contact <a href="<?php echo $links['Krokedil']['href']; ?>" target="_blank">Krokedil</a>, the plugin's developer.</p>
+	<div id='krokedil-support-form'>
+		<h1 class="krokedil-support-form title">Technical support request form</h1>
+		<div class="krokedil-support-form">
+			<h2>E-mail <span class="required">(required)</span></h2>
+			<p>
+				<label for="email">Replies will be sent to this address, please check for typos.</label>
+				<input id="email" name="email" type="email" size="50" required>
+			</p>
+			<h2>Subject <span class="required">(required)</span></h2>
+			<p>
+				<label for="subject">Summarize your question in a few words.</label>
+				<input id="subject" name="subject" type="text" size="50" required>
+			</p>
+			<h2>How can we help? <span class="required">(required)</span></h2>
+			<p>
+				<label for="description">Describe the issue you are having as detailed as possible.</label>
+				<textarea id="description" name="description" rows="4" cols="70" required></textarea>
+			</p>
+			<h2>WooCommerce system status report <span class="woocommerce-help-tip"></span></h2>
+			<p>This report contains information about your website that is very useful for troubleshooting issues.</p>
+			<div class="system-report-wrapper">
+				<input name="system-report-include" type="checkbox" checked>
+				<label id="system-report-include">Attach this store's WooCommerce system status report.</label>
+				<a href="#" class="system-report-action">View report</a>
+				<textarea class="system-report-content" readonly></textarea>
+			</div>
+			<h2>WooCommerce logs <span class="woocommerce-help-tip"></span></h2>
+			<p>
+				If you have logging enabled, providing relevant logs can be very useful for troubleshooting (e.g., issue with a specific order).
+			</p>
+			<div class="woocommerce-log">
+				<?php
+				Krokedil_Support_Form::output_log_selector( 'Klarna Checkout', $logs['kco'] );
+				Krokedil_Support_Form::output_log_selector( 'Klarna Order Management', $logs['kom'] );
+				Krokedil_Support_Form::output_log_selector( 'Fatal error log', $logs['fatal'] );
+				Krokedil_Support_Form::output_log_selector( 'Additional log', $logs['all'] );
+
+				echo '<a class="additional-log">+Add an additional log</a>';
+				for ( $i = 1; $i <= count( $logs ); $i++ ) {
+					Krokedil_Support_Form::output_log_selector( "Additional log {$i}", $logs['all'] );
+					if ( $i < count( $logs ) ) {
+						echo "<a class='additional-log-{$i}'>+Add an additional log</a>";
+					}
+				}
+
+				?>
+			</div>
+			<div class="uploads">
+				<h1>Upload screenshots</h1>
+				<p>Add any relevant screenshots, e.g., of an order related to the problem.</p>
+				<p>Please submit screenshots from both the WooCommerce admin, and the payment provider's portal with theorder notes clearly visible in the screenshot.</p>
+				<input type="file" name="screenshots[]" id="screenshot-picker" accept=".jpeg,.jpg,.png,.gif" multiple>
+				<input type="submit" value="Submit support ticket" name="submit" class="button button-primary button-large">
+			</div>
+			<input type="hidden" name="krokedil_support_form_nonce" value="<?php echo wp_create_nonce( 'krokedil_support_form_nonce' ); ?>">
+		</div>
+	</div>
 </div>
