334379d382e71460e6f6a06ef59a64d3cb4efaf6
Johan Falk <johan@krokedil.se>
2023-03-27 18:50:22 +0200

    Various css tweak and minor text change.

--- includes/admin/views/html-kco-support.php
+++ includes/admin/views/html-kco-support.php
@@ -21,6 +21,6 @@ $GLOBALS['hide_save_button'] = true;
 		<li><a href="<?php echo $links['Technical documentation']['href']; ?>" target="_blank">Technical documentation</a></li>
 		<li><a href="<?php echo $links['General support information']['href']; ?>" target="_blank">General support information</a></li>
 	</ul>
-	<p>If you have questions regarding a certain purchase, you're welcome to contact <a href="<?php echo $links['Klarna']['href']; ?>" target="_blank">Klarna.</a></p>
+	<p>If you have questions regarding a certain purchase, you're welcome to contact <a href="<?php echo $links['Klarna']['href']; ?>" target="_blank">Klarna</a>.</p>
 	<p>If you have <b>technical questions or questions regarding configuration</b> of the plugin, you're welcome to contact <a href="<?php echo $links['Krokedil']['href']; ?>" target="_blank">Krokedil</a>, the plugin's developer.</p>
 </div>
