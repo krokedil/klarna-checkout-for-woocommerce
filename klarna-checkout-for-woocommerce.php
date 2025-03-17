72ad79b4eb389dd5264f64c5755818a6145c483b
anyakro <anya@krokedil.se>
2025-03-17 11:59:41 +0100

    Version 2.13.5
    * Fix - Fixed checkout scrollTo animations that were sometimes queued multiple times, potentially causing the checkout page to freeze - Thank you Oxyc for the fix!
    * Fix - Fixed an issue where KCO-related JS was loaded even when no payment option was available at checkout.

--- klarna-checkout-for-woocommerce.php
+++ klarna-checkout-for-woocommerce.php
@@ -5,7 +5,7 @@
  * Description: Klarna Checkout payment gateway for WooCommerce.
  * Author: Klarna
  * Author URI: https://klarna.com/
- * Version: 2.13.4
+ * Version: 2.13.5
  * Text Domain: klarna-checkout-for-woocommerce
  * Domain Path: /languages
  *
@@ -35,7 +35,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Required minimums and constants
  */
-define( 'KCO_WC_VERSION', '2.13.4' );
+define( 'KCO_WC_VERSION', '2.13.5' );
 define( 'KCO_WC_MIN_PHP_VER', '5.6.0' );
 define( 'KCO_WC_MIN_WC_VER', '3.9.0' );
 define( 'KCO_WC_MAIN_FILE', __FILE__ );
