e377518b4418c15dd43abf5da783f749d1007245
MichaelBengtsson <michael@krokedil.se>
2025-07-17 14:54:42 +0200

    Run build command for blocks and other assets

---
+++ blocks/build/checkout.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-polyfill'), 'version' => 'ce923b0cd2ec9bb12100');
