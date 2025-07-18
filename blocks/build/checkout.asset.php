b7fe0b458dbc372eab0eb3aa3fdff5d533edde54
MichaelBengtsson <michael@krokedil.se>
2025-07-18 15:23:50 +0200

    Update build scripts to use wp-scripts functionality instead.

--- blocks/build/checkout.asset.php
+++ blocks/build/checkout.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-polyfill'), 'version' => '4ce6a53f1cc32a5cc584');
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-polyfill'), 'version' => '98ea0f561438b3d2dc35');
