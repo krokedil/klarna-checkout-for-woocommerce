22a88b7a73783a7500cdc4af643e7a8348476361
MichaelBengtsson <michael@krokedil.se>
2026-04-02 16:55:14 +0200

    Only enable upsell if setting is enabled

--- src/Upsell/UpsellHooks.php
+++ src/Upsell/UpsellHooks.php
@@ -16,8 +16,22 @@ class UpsellHooks {
 
 		// Only add hooks if upsell is enabled in the settings.
 		if ( wc_string_to_bool( $settings['enable_upsell'] ?? 'no' ) ) {
+			add_filter( 'kco_wc_merchant_urls', array( $this, 'add_upsell_urls' ) );
 			add_filter( 'woocommerce_hidden_order_itemmeta', array( $this, 'add_hidden_order_itemmeta' ) );
 		}
+	}
+
+	/**
+	 * Add the upsell and upsell validation URLs to the list of merchant URLs.
+	 *
+	 * @param array $merchant_urls The list of merchant URLs.
+	 *
+	 * @return array
+	 */
+	public function add_upsell_urls( $merchant_urls ) {
+		$merchant_urls['upsell_validation'] = home_url( '/wc-api/KCO_WC_Upsell_Validation/?kco_wc_order_id={checkout.order.id}' );
+
+		return $merchant_urls;
 	}
 
 	/**
