5816209bcadc1b14d5a63731865749ecaaec222f
anyakro <anya@krokedil.se>
2026-06-08 11:01:18 +0200

    Custom handling of empty body for update/retrieve

--- classes/requests/checkout/post/class-kco-request-update.php
+++ classes/requests/checkout/post/class-kco-request-update.php
@@ -131,4 +131,43 @@ class KCO_Request_Update extends KCO_Request {
 			'timeout'    => apply_filters( 'kco_wc_request_timeout', 10 ),
 		);
 	}
+
+	/**
+	 * Checks response for any error.
+	 *
+	 * @param object $response The response.
+	 * @param array  $request_args The request args.
+	 * @param string $request_url The request URL.
+	 * @return object|array
+	 */
+	public function process_response( $response, $request_args = array(), $request_url = '' ) {
+		// Check if response is a WP_Error, and return it back if it is.
+		if ( is_wp_error( $response ) ) {
+			return $response;
+		}
+
+		$body = wp_remote_retrieve_body( $response );
+		$code = wp_remote_retrieve_response_code( $response );
+		// Check the status code, if its not between 200 and 299 then its an error.
+		if ( $code < 200 || $code > 299 ) {
+			$data          = 'URL: ' . $request_url . ' - ' . wp_json_encode( $this->sanitize_request_args( $request_args ) );
+			$error_message = '';
+			// Get the error messages.
+			$errors = json_decode( $body, true );
+			if ( empty( $errors ) ) {
+
+				if ( 404 === $code ) {
+					return new WP_Error( $code, 'The order was not found in Kustom. Attempting to update the order again.', $data );
+				}
+
+				return new WP_Error( $code, 'received empty body', $data );
+			} elseif ( isset( $errors['error_messages'] ) && is_array( $errors['error_messages'] ) ) {
+				foreach ( $errors['error_messages'] as $error ) {
+					$error_message = "$error_message  $error";
+				}
+			}
+			return new WP_Error( $code, "$body $error_message", $data );
+		}
+		return json_decode( $body, true );
+	}
 }
