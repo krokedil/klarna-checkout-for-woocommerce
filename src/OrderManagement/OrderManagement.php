3a878ace166bc124a768313c74c30fb60bdaf4dd
Montazar <github.mdtk4@passmail.com>
2026-08-23 23:42:22 +0200

    Update OrderManagement.php

--- src/OrderManagement/OrderManagement.php
+++ src/OrderManagement/OrderManagement.php
@@ -117,7 +117,7 @@ class OrderManagement {
 		// Update an order.
 		add_action( 'woocommerce_saved_order_items', array( $this, 'update_klarna_order_items' ), 10, 2 );
 
-		// Refund an order. Runs last so another plugin on this filter can't discard our result.
+		// Refund an order. Runs after lower-priority callbacks so they can't discard our result.
 		add_filter( 'wc_klarna_checkout_process_refund', array( $this, 'refund_klarna_order' ), PHP_INT_MAX, 4 );
 
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin' ) );
