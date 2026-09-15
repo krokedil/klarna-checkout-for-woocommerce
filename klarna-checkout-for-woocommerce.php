84cd89487329ce7f5e51747ff18c50c7111f9877
Montazar <github.mdtk4@passmail.com>
2026-09-15 10:25:18 +0200

    Version 2.21.0

--- klarna-checkout-for-woocommerce.php
+++ klarna-checkout-for-woocommerce.php
@@ -5,12 +5,12 @@
  * Description: Kustom Checkout payment gateway for WooCommerce.
  * Author: Kustom
  * Author URI: https://klarna.com/
- * Version: 2.20.11
+ * Version: 2.21.0
  * Text Domain: klarna-checkout-for-woocommerce
  * Domain Path: /languages
  *
  * WC requires at least: 9.7.0
- * WC tested up to: 11.0.1
+ * WC tested up to: 11.1.0
  *
  * Copyright (c) 2017-2026 Krokedil
  *
@@ -41,7 +41,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Required minimums and constants
  */
-define( 'KCO_WC_VERSION', '2.20.11' );
+define( 'KCO_WC_VERSION', '2.21.0' );
 define( 'KCO_WC_MIN_PHP_VER', '5.6.0' );
 define( 'KCO_WC_MIN_WC_VER', '3.9.0' );
 define( 'KCO_WC_MAIN_FILE', __FILE__ );
